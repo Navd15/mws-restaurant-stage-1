e95dde171e938604632db4dd1d88d975cf7865d8
Navd15 <gill.222navdeep@gmail.com>
2018-07-07 23:46:24 +0530

    fix:added aria-label to view details button

--- js/main.js
+++ js/main.js
@@ -184,6 +184,7 @@ const img=document.createElement('img');
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label',`View Details of ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
