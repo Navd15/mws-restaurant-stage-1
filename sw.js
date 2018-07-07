dba7c02e20383a892be78c4c141fff8e8231ac5b
Navd15 <gill.222navdeep@gmail.com>
2018-07-07 23:45:49 +0530

    fix: sw caching problem

--- sw.js
+++ sw.js
@@ -2,23 +2,17 @@ const cacheName="rest-cache-v2";
 
 self.addEventListener('install',(event)=>{
         event.waitUntil(caches.open(cacheName).then((cache)=>{
-    return cache.addAll(["/", 
-             "/data/restaurants.json",
+    return cache.addAll([
+        "/js/main.js",
+        "/index.html",
+        "/js/dbhelper.js",
+        "/js/restaurant_info.js",
+            "/data/restaurants.json",
             "/css/styles.css",
-             "/css/@media768.css", 
-             "/css/@media425.css",
-             "/restaurant.html",
-             "/favicon.ico",
-            // "/img/1.jpg",
-            // "/img/2.jpg",
-            // "/img/3.jpg",
-            // "/img/4.jpg",
-            // "/img/5.jpg",
-            // "/img/6.jpg",
-            // "/img/7.jpg",
-            // "/img/8.jpg",
-            // "/img/9.jpg",
-            // "/img/10.jpg",
+            "/css/@media768.css", 
+            "/css/@media425.css",
+            "/favicon.ico",
+        
         ]) 
         })
     
